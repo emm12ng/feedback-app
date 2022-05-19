66194f24f9cb25cacf9d4916f287233f245e0808
Emma Nguyen <emm12nguyen@gmail.com>
2022-05-19 18:51:53 -0400

    5/19

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'fdjkasdf'
+    },
+    {
+        id: 2,
+        rating: 8,
+        text: 'fdjkasdfasdfdf'
+    },
+    {
+        id: 3,
+        rating: 3,
+        text: 'fdjkasdfasdjdklasjdkffdf'
+    },
+]
+
+export default FeedbackData
